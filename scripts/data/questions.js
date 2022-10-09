a697db000a2ce8130f2c872e2c10db0d4bfb4c84
kadekdarmayasa <darmayasadiputra@gmail.com>
2022-10-09 10:38:29 +0800

    Spin wheel game

---
+++ scripts/data/questions.js
@@ -0,0 +1,32 @@
+const questions = {
+	opsiA: [
+		'Jelaskan 4 tipe sifat proses produksi!',
+		'Jelaskan jenis - jenis proses produksi berdasarkan wujud produksi!',
+		'Jelaskan jenis - jenis proses produksi berdasarkan segi alur proses produksi!',
+		'Jelaskan tahapan produksi dalam bidang RPL!',
+		'Jelaskan tahapan pembuatan animasi!',
+	],
+	opsiB: [
+		'Jelaskan 4 tipe sifat proses produksi!',
+		'Jelaskan jenis - jenis proses produksi berdasarkan wujud produksi!',
+		'Jelaskan jenis - jenis proses produksi berdasarkan segi alur proses produksi!',
+		'Jelaskan tahapan produksi dalam bidang RPL!',
+		'Jelaskan tahapan pembuatan animasi!',
+	],
+	opsiC: [
+		'Jelaskan 4 tipe sifat proses produksi!',
+		'Jelaskan jenis - jenis proses produksi berdasarkan wujud produksi!',
+		'Jelaskan jenis - jenis proses produksi berdasarkan segi alur proses produksi!',
+		'Jelaskan tahapan produksi dalam bidang RPL!',
+		'Jelaskan tahapan pembuatan animasi!',
+	],
+	opsiD: [
+		'Jelaskan 4 tipe sifat proses produksi!',
+		'Jelaskan jenis - jenis proses produksi berdasarkan wujud produksi!',
+		'Jelaskan jenis - jenis proses produksi berdasarkan segi alur proses produksi!',
+		'Jelaskan tahapan produksi dalam bidang RPL!',
+		'Jelaskan tahapan pembuatan animasi!',
+	],
+};
+
+export default questions;
